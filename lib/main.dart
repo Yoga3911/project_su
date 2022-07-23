a71d9722504c5fb68a8bc7a02e7714395e9ccc1d
Robin Hood <yoga3911@gmail.com>
2022-07-23 23:08:14 +0700

    add firebase init

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
+import 'firebase_options.dart';
 import 'presentation/providers/init.dart';
 import 'routes/routes.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
