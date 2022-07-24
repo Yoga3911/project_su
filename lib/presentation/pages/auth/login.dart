52aff9cd5734c6b4ad252a0adaef692cabb7c0fb
Robin Hood <yoga3911@gmail.com>
2022-07-24 17:24:00 +0700

    update login page

--- lib/presentation/pages/auth/login.dart
+++ lib/presentation/pages/auth/login.dart
@@ -1,14 +1,20 @@
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatelessWidget {
-  const LoginPage({super.key});
+  final String name;
+  const LoginPage({
+    super.key,
+    required this.name,
+  });
 
-  static const routeName = "login-page";
+  static const routeName = "login_page";
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text("Login!"),
+    return Scaffold(
+      body: Center(
+        child: Text(name),
+      ),
     );
   }
 }
