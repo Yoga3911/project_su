f78f2c256890bda68a0d6e735fe56f7a850d2816
Robin Hood <yoga3911@gmail.com>
2022-07-24 14:48:03 +0700

    add route name

--- lib/presentation/pages/auth/register.dart
+++ lib/presentation/pages/auth/register.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 class RegisterPage extends StatelessWidget {
   const RegisterPage({super.key});
 
+  static const routeName = "register-page";
+
   @override
   Widget build(BuildContext context) {
     return const Center(
