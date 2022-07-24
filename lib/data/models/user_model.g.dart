1f1d4c41cbd0ce82c8ae5290ef928617f3637d03
Robin Hood <yoga3911@gmail.com>
2022-07-24 18:20:25 +0700

    json serialize model

---
+++ lib/data/models/user_model.g.dart
@@ -0,0 +1,18 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'user_model.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+_$_UserModel _$$_UserModelFromJson(Map<String, dynamic> json) => _$_UserModel(
+      name: json['name'] as String,
+      address: json['address'] as String,
+    );
+
+Map<String, dynamic> _$$_UserModelToJson(_$_UserModel instance) =>
+    <String, dynamic>{
+      'name': instance.name,
+      'address': instance.address,
+    };
