6b30d7de2eec4878a299c9dece7d0b25f0a0dfb5
Robin Hood <yoga3911@gmail.com>
2022-07-24 14:47:34 +0700

    switch to go router

--- lib/routes/routes.dart
+++ lib/routes/routes.dart
@@ -1,18 +1,54 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import 'package:project/presentation/pages/auth/login.dart';
 import 'package:project/presentation/pages/auth/register.dart';
 import 'package:project/presentation/widgets/main.dart';
 
 class MyRoute {
+  static final _single = MyRoute._();
   MyRoute._();
+  factory MyRoute() => _single;
 
+  static const routeName = "root";
+
+  static const root = "/";
   static const main = "/main";
-  static const login = "/login";
-  static const register = "/register";
+  static const login = "/auth/login";
+  static const register = "/auth/register";
 
-  static final data = <String, WidgetBuilder>{
-    main: (_) => const MainPage(),
-    login: (_) => const LoginPage(),
-    register: (_) => const RegisterPage(),
-  };
+  late final data = GoRouter(
+    debugLogDiagnostics: true,
+    initialLocation: root,
+    routes: <GoRoute>[
+      GoRoute(
+        path: root,
+        name: routeName,
+        redirect: (state) => state.namedLocation(MainPage.routeName),
+      ),
+      GoRoute(
+        path: login,
+        name: LoginPage.routeName,
+        pageBuilder: (_, state) => MaterialPage(
+          key: state.pageKey,
+          child: const LoginPage(),
+        ),
+      ),
+      GoRoute(
+        path: register,
+        name: RegisterPage.routeName,
+        pageBuilder: (_, state) => MaterialPage(
+          key: state.pageKey,
+          child: const RegisterPage(),
+        ),
+      ),
+      GoRoute(
+        path: main,
+        name: MainPage.routeName,
+        pageBuilder: (_, state) => MaterialPage(
+          key: state.pageKey,
+          child: const MainPage(),
+        ),
+      ),
+    ],
+  );
 }
