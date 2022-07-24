a0a939204af57e074cd8fdc4a76e3c46cfb78644
Robin Hood <yoga3911@gmail.com>
2022-07-24 14:48:23 +0700

    add my route to provider

--- lib/presentation/providers/init.dart
+++ lib/presentation/providers/init.dart
@@ -2,12 +2,17 @@ import 'package:project/presentation/providers/page_provider.dart';
 import 'package:provider/provider.dart';
 import 'package:provider/single_child_widget.dart';
 
+import '../../routes/routes.dart';
+
 class MyProvider {
   MyProvider._();
 
   static final data = <SingleChildWidget>[
     ChangeNotifierProvider(
       create: (_) => PageProvider(),
-    )
+    ),
+    Provider(
+      create: (_) => MyRoute(),
+    ),
   ];
 }
