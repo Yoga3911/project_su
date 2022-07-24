4a4fe139ddff5f5806383ba823bffc16f3da3db5
Robin Hood <yoga3911@gmail.com>
2022-07-24 17:24:08 +0700

    update register page

--- lib/presentation/pages/auth/register.dart
+++ lib/presentation/pages/auth/register.dart
@@ -3,12 +3,14 @@ import 'package:flutter/material.dart';
 class RegisterPage extends StatelessWidget {
   const RegisterPage({super.key});
 
-  static const routeName = "register-page";
+  static const routeName = "register_page";
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text("Register!"),
+    return const Scaffold(
+      body: Center(
+        child: Text("Register!"),
+      ),
     );
   }
 }
